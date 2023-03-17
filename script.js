8f3f609e686ab495f05da9090554cb53db5641da
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-17 14:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let arr = [2, 1, 2];
+
+let maxEle = 0;
+let ele = 0;
+let count = 0;
+
+function majorityEle(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i+1; j < arr.length; j++) {
+			if(arr[i] == arr[j]){
+				count++;
+				if(count > maxEle){
+					maxEle = count;
+					ele = arr[i];
+				}
+			}
+		}
+	}
+	return ele;
+}
